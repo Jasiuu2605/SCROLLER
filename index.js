fa2260617c0b4bf9e140abf24871d2ccfff670e3
Jasiuu2605 <98971547+Jasiuu2605@users.noreply.github.com>
2022-11-03 09:43:12 +0100

    add scroll feature

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-document.addEventListener('DOMContentLoaded', function() {
-    console.log("Hello world");
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Hello world");
 
+  const rootElement = document.querySelector("#root");
+  const sections = document.querySelectorAll("section");
+  let currentSectionIndex = 0;
+  let isThrotled = false;
 
-    const rootElement = document.querySelector('#root');
-    const sections = document.querySelectorAll('section');
+  document.addEventListener("mousewheel", function (event) {
+    if (isThrotled) {
+      return;
+    }
+    isThrotled = true;
 
-    document.addEventListener('mousewheel', function(event) {
-        console.log(event.wheelDelta);
-    })
-})
+    setTimeout(() => {
+      isThrotled = false;
+    }, 1000);
+
+    const direction = event.wheelDelta < 0 ? 1 : -1;
+
+    if (direction === 1) {
+      const isLastSection = currentSectionIndex === sections.length - 1;
+      if (isLastSection) {
+        return;
+      }
+    } else if (direction === -1) {
+      const firstSection = currentSectionIndex === 0;
+      if (firstSection) {
+        return;
+      }
+    }
+    currentSectionIndex = currentSectionIndex + direction;
+
+    sections[currentSectionIndex].scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
